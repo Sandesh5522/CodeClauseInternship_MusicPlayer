aa0bc2bec1bbf1c784df600081caf3642dfc7b5f
Sandesh <sandeshsutar802@gmail.com>
2023-12-13 01:57:56 +0530

    trying code to find contents of folder

--- maincode.py
+++ maincode.py
@@ -1,13 +1,16 @@
 import PySimpleGUI as sg
-# import pygame as pg
+import pygame as pg
 import os
 
 sg.theme('DarkAmber')
 
+filenames = []
+
 layout = [
-    [sg.Text('Python Music Player!!')],
-    [sg.Image(), sg.FolderBrowse('FolderBrowse')],
-    [sg.Multiline(key = 'folder', size = (40,30))],#auto_refresh = True, auto_size_text = True, autoscroll = True
+    [sg.Text('Python Music Player!!', justification='center', size=(100,1))],
+    [sg.FolderBrowse(key = 'FolderBrowse'), sg.Text(key = 'path')],
+    [sg.Multiline(key = 'folder', size = (40,20)), sg.Listbox(filenames, key = 'folder', size = (40,20))],
+    [sg.Button('Prev'), sg.Button('Play'), sg.Button('Next')],
     [sg.Submit(), sg.Button('Cancel')]
 ]
 
@@ -19,12 +22,16 @@ while True:
         break
     elif event == 'Submit':
         foldername = values['FolderBrowse'] or '.'
-        filenames = os.listdir(foldername)
-        window['folder'].update("\n".join(filenames))
-        # print('ok')
+        # window['path'].update('selected folder path: ',foldername)
+        # filenames = os.listdir(foldername)
+        filenames = os.walk(foldername)
+        # window['folder'].update("\n".join(filenames))
+        window['folder'].update(filenames)
+        print(filenames)
+        window['path'].update('selected folder path: ',foldername)
 
-print("https://github.com/PySimpleGUI/PySimpleGUI/issues/4393#issuecomment-859296723")
-print("https://stackoverflow.com/questions/63725995/how-to-display-files-in-folder-when-using-pysimplegui-filebrowse-function")
+# https://github.com/PySimpleGUI/PySimpleGUI/issues/4393#issuecomment-859296723
+# https://stackoverflow.com/questions/63725995/how-to-display-files-in-folder-when-using-pysimplegui-filebrowse-function
 # To restrict a file type
 # layout =  [[sg.In() ,sg.FileBrowse(file_types=(("Text Files", "*.txt"),))]]
 window.close()
